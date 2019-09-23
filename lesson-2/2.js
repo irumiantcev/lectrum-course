efe41b75ecc9430c82137721973dc79c225ef98d
irumiantcev <rumis@live.ru>
2019-09-23 13:03:29 +0300

    2

--- lesson-2/2.js
+++ lesson-2/2.js
@@ -1,3 +1,5 @@
-var message = (login == 'Pitter') ? message = 'Hi'      :
-	      (login == 'Owner')  ? message = 'Hello'   :
-	      (login == '')       ? message = 'unknown' : message = '';
+var message = (login == 'Pitter') ? 'Hi'  :
+	          (login == 'Owner')  ? 'Hello'   :
+	          (login == '')       ? 'unknown' : '';
+
+console.log(message);
