af092355e50892c7c43ab7657e1e9d3b51accc1c
irumiantcev <rumis@live.ru>
2019-09-29 10:51:23 +0300

    lesson - 4

---
+++ lesson-4/1.js
@@ -0,0 +1,31 @@
+/**
+ * Задача 1.
+ *
+ * Создайте объект `person` c одним свойством `salary`.
+ * При чтении этого свойства должна возвращаться строка с текстом.
+ * Если до конца месяца осталось больше чем 20 дней — возвращается строка `good salary`, а если нет — `bad salary`
+ * 
+ * Условия:
+ * - Свойство salary обязательно должно быть геттером.
+ */
+
+const person = {};
+
+
+Object.defineProperty(person, 'salary', {
+	get() {
+		const today = new Date();
+		const lastDay = new Date(today.getFullYear(), today.getMonth(), 0);
+
+		return (lastDay.getDate() - today.getDate() > 20) ?  'good salary' : 'bad salary';
+	}
+});
+
+
+// Решение
+
+const example1 = person.salary;
+
+exports.person = person;
+
+console.log(example1);
