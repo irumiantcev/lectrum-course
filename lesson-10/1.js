6e6c4fea21b03aaf958df52c6a1d293f03479ff6
irumiantcev <rumis@live.ru>
2019-10-19 13:54:17 +0300

    lesson-10

---
+++ lesson-10/1.js
@@ -0,0 +1,53 @@
+/**
+ * Задача 1.
+ *
+ * Напишите имплементацию функции Function.prototype.bind().
+ *
+ * Наша собственная функция bind() должна задавать контекст выполнения целевой функции,
+ * и возвращать новую, привязанную версию целевой функции.
+ *
+ * Функция bind() должна обладать следующими параметрами:
+ * - Первый параметр — это функция, контекст выполнения которой мы хотим привязать;
+ * - Второй параметр — это объект (не массив), в контексте которого нужно вызывать функцию из первого параметра;
+ * - Третий и все остальные параметры — это аргументы для вызова функции из первого параметра.
+ *
+ * Генерировать ошибки если:
+ * - Первый параметр не является типом function;
+ * - Второй параметр не является типом object;
+ * - Второй параметр является массивом.
+ *
+ * Условия:
+ * - Использовать встроенную функцию Function.prototype.bind() запрещено.
+ */
+
+// Решение
+
+const bind = (func, object, ...args) => {
+    if (typeof func !== 'function') {
+        throw new Error (`argument ${func} is not a function`);
+    }   
+
+    if (typeof object !== 'object' || Array.isArray(object)) {
+        throw new Error (`argument ${object} must be an object`);
+    }
+
+    const resultFunction = () => func.call(object, ...args);
+
+    return resultFunction;
+}
+
+
+function getName(greeting, message) {
+    return `${greeting} ${message} ${this.name}.`;
+}
+
+const user = { name: 'Walter', getName };
+const oliver = { name: 'Oliver' };
+
+const boundedGetName = bind(getName, oliver, 'Hello!', 'I am');
+
+console.log(user.getName('Hello!', 'My name is')); // Hello! My name is Walter.
+console.log(boundedGetName()); // Hello! I am Oliver.
+
+exports.bind = bind;
+
---
+++ lesson-10/2.js
@@ -0,0 +1,56 @@
+/**
+ * Задача 2.
+ *
+ * Напишите функцию calculate(), которая в качестве аргументов принимает неограниченное количество функций.
+ *
+ * При запуске calculate() последовательно запускает коллбек-функции из аргументов.
+ * Каждая коллбек-функция из цепочки в качестве своего аргумента принимает то, что возвращает предыдущая коллбек-функция.
+ * То есть возвращаемое значение каждой коллбек-функции из цепочки
+ * становится доступным из параметра следующей коллбек-функции в цепочке.
+ *
+ * Первая коллбек-функция не принимает параметров.
+ *
+ * Функция calculate() должна вернуть результат выполнения последней коллбек-функции из цепочки.
+ *
+ * Генерировать ошибки если:
+ * - Любой из аргументов функции calculate() не является функцией;
+ * - Любая функция из аргументов не вернула значение.
+ */
+
+// Решение
+
+const calculate = (...args) => {
+    let result;
+
+    for (const func of args) {
+        if (typeof func !== 'function') {
+            throw new Error(`Argument ${func} is not a function`);
+        }
+
+        if (func() === undefined) {
+            throw new Error(`Function ${func} doesn't return anything`);
+        }
+
+        result = func(result);
+    }
+
+    return result;
+}
+
+
+
+const result = calculate(
+    () => {
+        return 7;
+    },
+    prevResult => {
+        return prevResult + 4;
+    },
+    prevResult => {
+        return prevResult * 5;
+    },
+);
+
+console.log(result); // 55
+
+exports.calculate = calculate;
